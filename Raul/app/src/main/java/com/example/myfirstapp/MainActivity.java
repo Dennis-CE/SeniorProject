01f0a46a9ba75cf67a79809f9fb087c85626a667
Raul Solorio <rauljr1910@yahoo.com>
2019-10-05 21:36:39 -0700

    Added comment to code to see how to commit on bash

--- Raul/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ Raul/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// commented to test how to use bash
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
