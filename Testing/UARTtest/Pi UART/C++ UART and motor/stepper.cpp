09448fb4841d45cd7c95f1bec9d9067c47282da6
Raul Solorio <rauljr1910@yahoo.com>
2020-03-24 18:32:45 -0700

    Removed 1 slot/motor code. Fixed comments.

--- Testing/UARTtest/Pi UART/C++ UART and motor/stepper.cpp
+++ Testing/UARTtest/Pi UART/C++ UART and motor/stepper.cpp
@@ -14,10 +14,7 @@ int main()
 	pinMode(2,OUTPUT); 		       // GPIO27 is ~EN (turn on/off motor) --> MOTOR 1
 	pinMode(3,OUTPUT); 		       // GPIO22 STEP
 	pinMode(4,OUTPUT);                     // GPIO23 DIR
-	pinMode(5,OUTPUT);                     // GPIO24 ~EN ; MOTOR 2
-	pinMode(6,OUTPUT);                     // GPIO25 STEP
-	pinMode(7,OUTPUT);                     // GPIO4 DIR
-	pinMode(8,OUTPUT);                     // GPIO2 ~EN; MOTOR 3		
+	pinMode(5,OUTPUT);                     // GPIO24 ~EN ; MOTOR 2	
 
 	while(1)
 	{
@@ -27,20 +24,6 @@ int main()
 		
 		if(slot == 1)
 		{
-			/*
-				CLOCKWISE
-			*/
-			digitalWrite(2,0); 	   // motor on
-			digitalWrite(1,0);	   // motor spins clockwise
-			for(int x=0;x<50;x++) 	   // spin 90 degrees
-			{
-				digitalWrite(0,1); // step
-				delay(1);	   // wait 1ms
-				digitalWrite(0,0); // see the stepping transition (1->0)
-				delay(1);	   // wait 1ms
-			}
-			digitalWrite(2,1); 	   // turn off motor
-			delay(1000);		   // wait 1000  milliseconds
 			/*
 				COUNTER-CLOCKWISE
 			*/			
@@ -55,21 +38,24 @@ int main()
 			}
 		        digitalWrite(2,1);	   // turn off motor
 			delay(1000);		   // wait 1000 millisecond
-			serialPuts(fd,"f30\r");	   // sends string "f30" via UART...TM4C needs to see 'ENTER' aka \r\n towork
+			/*
+				CLOCKWISE
+			*/
+			digitalWrite(2,0); 	   // motor on
+			digitalWrite(1,0);	   // motor spins clockwise
+			for(int x=0;x<50;x++) 	   // spin 90 degrees
+			{
+				digitalWrite(0,1); // step
+				delay(1);	   // wait 1ms
+				digitalWrite(0,0); // see the stepping transition (1->0)
+				delay(1);	   // wait 1ms
+			}
+			digitalWrite(2,1); 	   // turn off motor
+			delay(1000);		   // wait 1000  milliseconds
+			serialPuts(fd,"f30\r");	   // sends string "f30" via UART...TM4C needs to see 'ENTER' aka \r to work
 		}
 		else if(slot == 2)
 		{
-			digitalWrite(5,0);	   // motor on
-			digitalWrite(4,0);	   // spin clockwise
-			for(int x=0; x<50; x++)    // spin 90 degrees
-			{
-				digitalWrite(3,1); // wait 1ms
-				delay(1);          // wait 1ms
-				digitalWrite(3,0); // wait 1ms
-				delay(1);	   // wait 1ms
-			}
-			digitalWrite(5,1);	   // turn off motor
-			delay(1000);		   // wait 1000ms
 			digitalWrite(5,0);	   // turn on motor
 			digitalWrite(4,1);	   // motor spins counter-clockwise
 			for(int x=0; x<50; x++)    // spin 90 degrees
@@ -81,32 +67,18 @@ int main()
 			}
 		        digitalWrite(5,1);	   // turn off motor
 			delay(1000);		   // wait 1000 milliseconds
-			serialPuts(fd,"f30\r");	   // sends string "f30" via UART...TM4C needs to see 'ENTER' aka \r\n towork
-		}
-		else if(slot == 3)
-		{	
-			digitalWrite(8,0);	   // turn motor on
-			digitalWrite(7,0);	   // turn clockwise
-			for(int x=0; x<50;x++){    // 90 degrees
-				digitalWrite(6,1); // step
-				delay(1);	   // wait 1ms
-				digitalWrite(6,0); // see the stepping transition
-				delay(1);	   // wait 1ms
-			}
-			digitalWrite(8,1);
-			delay(1000);
-			digitalWrite(8,0);	   // turn on motor
-			digitalWrite(7,1);	   // motor spins counter-clockwise
-			for(int x=0; x<50; x++)    // spin 90 dgrees
+			digitalWrite(5,0);	   // motor on
+			digitalWrite(4,0);	   // spin clockwise
+			for(int x=0; x<50; x++)    // spin 90 degrees
 			{
-				digitalWrite(6,1); // step
-				delay(1);	   // wait 1ms
-				digitalWrite(6,0); // see the stepping transition
+				digitalWrite(3,1); // wait 1ms
+				delay(1);          // wait 1ms
+				digitalWrite(3,0); // wait 1ms
 				delay(1);	   // wait 1ms
 			}
-		        digitalWrite(8,1);	   // turn off motor
-			delay(1000);		   // wait 1000 milliseconds
-			serialPuts(fd,"f30\r");	   // sends string "f30" via UART...TM4C needs to see 'ENTER' aka \r\n towork
+			digitalWrite(5,1);	   // turn off motor
+			delay(1000);		   // wait 1000ms			
+			serialPuts(fd,"f30\r");	   // sends string "f30" via UART...TM4C needs to see 'ENTER' aka \r to work
 		}
 		else
 		{
@@ -114,6 +86,5 @@ int main()
 			digitalWrite(5,1);	   // turn off motor
 			digitalWrite(8,1);	   // turn off motor
 		}
-
 	}
 }
